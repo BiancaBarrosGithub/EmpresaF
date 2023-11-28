3c5078a080c33cdc1cf903038ac30269b2d76d40
BiancaBarrosGithub <146129798+BiancaBarrosGithub@users.noreply.github.com>
2023-11-28 14:16:15 -0300

    EmpresaF

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule} from '@angular/forms';
+import { FormEmpresaComponent } from './form-empresa/form-empresa.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormEmpresaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
